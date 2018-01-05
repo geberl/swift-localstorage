ea774e30ab0f1552e748d71d020b4e2e329eeacf
Günther Eberl <guenther@eberl.se>
2018-01-05 21:31:05 +0100

    Moved refresh function to common
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- LocalStorage/localstorage/Common.swift
+++ LocalStorage/localstorage/Common.swift
@@ -113,3 +113,76 @@ func removeDir(path: String) {
         print(error.localizedDescription)
     }
 }
+
+
+func refreshStats() {
+    print("refreshStats")
+    
+    resetAppState()
+    
+    AppState.documentsPath = FileManager.documentsDir()
+    print("Examining '" + AppState.documentsPath + "'")
+    
+    let fileManager = FileManager.default
+    guard let enumerator: FileManager.DirectoryEnumerator = fileManager.enumerator(atPath: AppState.documentsPath) else {
+        print("Directory not found!")
+        return
+    }
+    
+    while let element = enumerator.nextObject() as? String {
+        var elementURL: URL = URL(fileURLWithPath: AppState.documentsPath)
+        elementURL.appendPathComponent(element)
+        
+        var elementIsTrashed: Bool
+        if element.starts(with: ".Trash") {
+            elementIsTrashed = true
+        } else {
+            elementIsTrashed = false
+        }
+        
+        var fileSize : UInt64
+        do {
+            let attr = try fileManager.attributesOfItem(atPath: elementURL.path)
+            fileSize = attr[FileAttributeKey.size] as! UInt64
+            // Note: FileAttributeKey.type is useless, just contains file/folder, not UTI.
+        } catch {
+            fileSize = 0
+            print("Error: \(error)")
+        }
+        
+        //let fileType: String = elementURL.typeIdentifier!
+        
+        // print(element + " (" + String(fileSize) + ") (" + fileType + ")")
+        
+        // examples: (16066) = 16KB, (57110) = 57KB, (1650104250) = 1,65GB
+        // only works for files
+        // folders show (64) or (96), so no contained files are summed into that, this is just the name string
+        
+        // put notice of how to manage files in there: in the files app
+        
+        if let values = try? elementURL.resourceValues(forKeys: [.isDirectoryKey]) {
+            if values.isDirectory! {
+                if elementIsTrashed {
+                    if element != ".Trash" {
+                        // Don't count the .Trash folder itself towards the folder count
+                        AppState.trashFoldersNumber += 1
+                    }
+                    AppState.trashSizeDiskBytes += fileSize
+                } else {
+                    AppState.localFoldersNumber += 1
+                    AppState.localSizeDiskBytes += fileSize
+                }
+            } else {
+                if elementIsTrashed {
+                    AppState.trashFilesNumber += 1
+                    AppState.trashSizeBytes += fileSize
+                    AppState.trashSizeDiskBytes += fileSize
+                } else {
+                    AppState.localFilesNumber += 1
+                    AppState.localSizeBytes += fileSize
+                    AppState.localSizeDiskBytes += fileSize
+                }
+            }
+        }
+    }
+}
