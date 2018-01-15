920f72710218851143a4dea11568e417397144d1
Günther Eberl <guenther@eberl.se>
2018-01-15 14:45:46 +0100

    Added Charts cocoapod and provided a basic implementation on the "Types" tab
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- LocalStorage/Local Storage/Main/TypesViewController.swift
+++ LocalStorage/Local Storage/Main/TypesViewController.swift
@@ -6,10 +6,11 @@
 //  Copyright © 2018 Günther Eberl. All rights reserved.
 //
 
+import Charts
 import UIKit
 import os.log
 
-class TypesViewController: UIViewController {
+class TypesViewController: UIViewController, ChartViewDelegate {
     
     let userDefaults = UserDefaults.standard
     
@@ -18,6 +19,8 @@ class TypesViewController: UIViewController {
     }
     
     @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var chartView: HorizontalBarChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +31,54 @@ class TypesViewController: UIViewController {
         
         self.setTheme()
         
-        print(AppState.typeSizes)
+        // data stuff below
+        //print(AppState.typeSizes)
+        
+        // chart stuff below
+        
+        chartView.chartDescription?.enabled = false
+        
+        chartView.dragEnabled = false
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled = false
+        
+        chartView.rightAxis.enabled = false
+        
+        chartView.delegate = self
+        
+        chartView.drawBarShadowEnabled = false
+        chartView.drawValueAboveBarEnabled = false
+        
+        chartView.maxVisibleCount = 60
+        
+        let xAxis = chartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.labelFont = .systemFont(ofSize: 10)
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = true
+        xAxis.granularity = 10
+        
+        let leftAxis = chartView.leftAxis
+        leftAxis.labelFont = .systemFont(ofSize: 10)
+        leftAxis.drawAxisLineEnabled = false
+        leftAxis.drawGridLinesEnabled = false
+        leftAxis.axisMinimum = 0
+        
+        let l = chartView.legend
+        l.horizontalAlignment = .left
+        l.verticalAlignment = .bottom
+        l.orientation = .horizontal
+        l.drawInside = false
+        l.form = .square
+        l.formSize = 8
+        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
+        l.xEntrySpace = 10
+        
+        chartView.fitBars = true
+        
+        chartView.animate(yAxisDuration: 2.0)
+        
+        self.setDataCount(Int(4), range: UInt32(50))
     }
 
     override func didReceiveMemoryWarning() {
@@ -63,6 +113,40 @@ class TypesViewController: UIViewController {
         let bgColor: UIColor = UIColor(named: bg)!
         
         self.mainView.backgroundColor = bgColor
+    }
+    
+    func setDataCount(_ count: Int, range: UInt32) {
+        let spaceForBar = 10.0
+        
+        let yVals = (0..<count).map { (i) -> BarChartDataEntry in
+            let mult = range + 1
+            let val = Double(arc4random_uniform(mult))
+            return BarChartDataEntry(x: Double(i) * spaceForBar, y: val)
+        }
+        
+        let set1 = BarChartDataSet(values: yVals, label: "Set1")
+        set1.drawIconsEnabled = false
+        
+        let audioVals = [BarChartDataEntry(x: Double(4) * spaceForBar, y: 5),
+                         BarChartDataEntry(x: Double(5) * spaceForBar, y: 10)]
+    
+        let audioSet = BarChartDataSet(values: audioVals, label: "Audio")
+        audioSet.drawIconsEnabled = false
+        audioSet.colors = [NSUIColor.red]
+        
+        let videoVals = [BarChartDataEntry(x: Double(6) * spaceForBar, yValues: [10, 40]),
+                         BarChartDataEntry(x: Double(7) * spaceForBar, yValues: [20, 40])]
+        
+        let videoSet = BarChartDataSet(values: videoVals, label: "Video")
+        videoSet.drawIconsEnabled = false
+        videoSet.colors = [NSUIColor.green, NSUIColor.blue]
+        videoSet.stackLabels = ["abc", "def"]
+        
+        let data = BarChartData(dataSets: [set1, audioSet, videoSet])
+        data.setValueFont(UIFont(name: "HelveticaNeue-Light", size: 10)!)
+        data.barWidth = 9.0
+        
+        chartView.data = data
     }
     
 }
