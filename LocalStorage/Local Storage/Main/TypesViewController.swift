83a879047b7572475963f890132e94044116bf83
Günther Eberl <guenther@eberl.se>
2018-01-18 09:19:44 +0100

    Updating values from the Types tab
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- LocalStorage/Local Storage/Main/TypesViewController.swift
+++ LocalStorage/Local Storage/Main/TypesViewController.swift
@@ -14,16 +14,15 @@ class TypesViewController: UIViewController, ChartViewDelegate, UITableViewDeleg
     
     let userDefaults = UserDefaults.standard
     
-    @IBAction func onSettingsButton(_ sender: UIButton) {
-        self.showSettings()
-    }
-    
     @IBOutlet var mainView: UIView!
-    
     @IBOutlet var chartView: HorizontalBarChartView!
-    
     @IBOutlet var typesTableView: UITableView!
     
+    @IBAction func onSettingsButton(_ sender: UIButton) {self.showSettings()}
+
+    @IBOutlet var refreshButton: UIButton!
+    @IBAction func onRefreshButton() {self.refresh()}
+
     override func viewDidLoad() {
         super.viewDidLoad()
         os_log("viewDidLoad", log: logGeneral, type: .debug)
@@ -31,77 +30,28 @@ class TypesViewController: UIViewController, ChartViewDelegate, UITableViewDeleg
         NotificationCenter.default.addObserver(self, selector: #selector(TypesViewController.setTheme),
                                                name: .darkModeChanged, object: nil)
         
+        NotificationCenter.default.addObserver(self, selector: #selector(TypesViewController.updatePending),
+                                               name: .updatePending, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(TypesViewController.updateValues),
+                                               name: .updateFinished, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(TypesViewController.updateValues),
+                                               name: .unitChanged, object: nil)
+        
         self.setTheme()
+        self.setupChart()
         
-        // Chart stuff below
-        
-        chartView.chartDescription?.enabled = false
-        
-        chartView.dragEnabled = false
-        chartView.setScaleEnabled(true)
-        chartView.pinchZoomEnabled = false
-        
-        chartView.rightAxis.enabled = false
-        
-        chartView.delegate = self
-        
-        chartView.drawBarShadowEnabled = false
-        chartView.drawValueAboveBarEnabled = false
-        
-        chartView.maxVisibleCount = 1
-        
-        let xAxis = chartView.xAxis
-        xAxis.labelPosition = .bottom
-        xAxis.labelFont = .systemFont(ofSize: 10)
-        xAxis.drawAxisLineEnabled = false
-        xAxis.drawGridLinesEnabled = false
-        xAxis.drawLabelsEnabled = false
-        xAxis.granularity = 10
-        
-        let leftAxis = chartView.leftAxis
-        leftAxis.labelFont = .systemFont(ofSize: 10)
-        leftAxis.axisLineColor = UIColor(named: "ColorFontGray")!
-        leftAxis.labelTextColor = UIColor(named: "ColorFontGray")!
-        leftAxis.drawAxisLineEnabled = false
-        leftAxis.drawGridLinesEnabled = false
-        leftAxis.drawLabelsEnabled = false
-        leftAxis.axisMinimum = 0
-        leftAxis.axisMaximum = 100
-        
-        let l = chartView.legend
-        l.horizontalAlignment = .left
-        l.verticalAlignment = .bottom
-        l.orientation = .horizontal
-        l.drawInside = false
-        l.form = .square
-        l.formSize = 8
-        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
-        l.textColor = UIColor(named: "ColorFontGray")!
-        l.xEntrySpace = 10
-        
-        chartView.fitBars = true
-        
-        if userDefaults.bool(forKey: UserDefaultStruct.animateUpdateDuringRefresh) {
-            chartView.animate(yAxisDuration: 1.5)
-        } else {
-            chartView.animate(yAxisDuration: 0)
+        if AppState.updateInProgress {
+            self.updatePending()
         }
-
-        self.setDataCount()
+        
+        self.updateGraph()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    func showSettings() {
-        os_log("showSettings", log: logUi, type: .debug)
-        
-        let storyboard = UIStoryboard(name: "Settings", bundle: nil)
-        let controller = storyboard.instantiateViewController(withIdentifier: "SettingsViewController")
-        self.view.setNeedsLayout()
-        self.view.setNeedsDisplay()
-        self.present(controller, animated: true, completion: nil)
+        os_log("didReceiveMemoryWarning", log: logGeneral, type: .info)
     }
     
     @objc func setTheme() {
@@ -117,14 +67,89 @@ class TypesViewController: UIViewController, ChartViewDelegate, UITableViewDeleg
     
     func applyColors(fg: String, bg: String) {
         os_log("applyColors", log: logGeneral, type: .debug)
-
+        
         // let fgColor: UIColor = UIColor(named: fg)!
         let bgColor: UIColor = UIColor(named: bg)!
         
         self.mainView.backgroundColor = bgColor
     }
     
-    func setDataCount() {
+    func showSettings() {
+        os_log("showSettings", log: logUi, type: .debug)
+        
+        let storyboard = UIStoryboard(name: "Settings", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "SettingsViewController")
+        self.view.setNeedsLayout()
+        self.view.setNeedsDisplay()
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    func refresh() {
+        os_log("refresh", log: logUi, type: .debug)
+        getStats()
+    }
+    
+    func setupChart() {
+        self.chartView.chartDescription?.enabled = false
+        self.chartView.dragEnabled = false
+        self.chartView.setScaleEnabled(true)
+        self.chartView.pinchZoomEnabled = false
+        self.chartView.rightAxis.enabled = false
+        self.chartView.delegate = self
+        self.chartView.drawBarShadowEnabled = false
+        self.chartView.drawValueAboveBarEnabled = false
+        self.chartView.maxVisibleCount = 1
+        
+        let xAxis = self.chartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.labelFont = .systemFont(ofSize: 10)
+        xAxis.drawAxisLineEnabled = false
+        xAxis.drawGridLinesEnabled = false
+        xAxis.drawLabelsEnabled = false
+        xAxis.granularity = 10
+        
+        let leftAxis = self.chartView.leftAxis
+        leftAxis.labelFont = .systemFont(ofSize: 10)
+        leftAxis.axisLineColor = UIColor(named: "ColorFontGray")!
+        leftAxis.labelTextColor = UIColor(named: "ColorFontGray")!
+        leftAxis.drawAxisLineEnabled = false
+        leftAxis.drawGridLinesEnabled = false
+        leftAxis.drawLabelsEnabled = false
+        leftAxis.axisMinimum = 0
+        leftAxis.axisMaximum = 100
+        
+        let l = self.chartView.legend
+        l.horizontalAlignment = .left
+        l.verticalAlignment = .bottom
+        l.orientation = .horizontal
+        l.drawInside = false
+        l.form = .square
+        l.formSize = 8
+        l.font = UIFont(name: "HelveticaNeue-Light", size: 11)!
+        l.textColor = UIColor(named: "ColorFontGray")!
+        l.xEntrySpace = 10
+        
+        self.chartView.fitBars = true
+        
+        if userDefaults.bool(forKey: UserDefaultStruct.animateUpdateDuringRefresh) {
+            self.chartView.animate(yAxisDuration: 1.5)
+        } else {
+            self.chartView.animate(yAxisDuration: 0)
+        }
+    }
+    
+    @objc func updatePending() {
+        os_log("updatePending", log: logGeneral, type: .debug)
+        
+        // TODO unclear what shows if this takes longer (copy 1000s of log files to iPad again)
+    }
+    
+    @objc func updateValues() {
+        self.updateGraph()
+        self.typesTableView.reloadData()
+    }
+    
+    func updateGraph() {
         let allSizes: Array = AppState.types.map { $0 .size }
         let sumSize: Double = Double(allSizes.reduce(0, +))
         let allSizesPercent: Array = allSizes.map { (Double($0) / sumSize) * 100 }
@@ -141,7 +166,7 @@ class TypesViewController: UIViewController, ChartViewDelegate, UITableViewDeleg
         data.setValueFont(UIFont(name: "HelveticaNeue-Light", size: 10)!)
         data.barWidth = 9.0
         
-        chartView.data = data
+        self.chartView.data = data
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
