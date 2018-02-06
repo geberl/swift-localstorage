675486ab900908a2be2c41b62910192678bc87c8
Günther Eberl <guenther@eberl.se>
2018-02-06 16:34:45 +0100

    Changed comment
    Signed-off-by: Günther Eberl <guenther@eberl.se>

--- LocalStorage/Local Storage/AppDelegate.swift
+++ LocalStorage/Local Storage/AppDelegate.swift
@@ -171,7 +171,7 @@ struct AppState {
     
     static var documentsPath: String = ""
     static var updateInProgress: Bool = false
-    static var demoContent: Bool = false  // NEVER activate this for a release!
+    static var demoContent: Bool = false  // NEVER set this to <true> when doing a release!
 }
 
 
